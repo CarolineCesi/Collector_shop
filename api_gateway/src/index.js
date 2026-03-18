272062ae66bb892cc097703eeb93ee91bbedddb3
caroline <caroline.grenet@viacesi.fr>
2026-03-18 21:25:32 +0100

    fix

--- api_gateway/src/index.js
+++ api_gateway/src/index.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 8080;
 
 // Middleware de sécurité
